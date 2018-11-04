37974abd510b10a6ee38e76107975a32ab1ee688
Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
2018-11-04 20:59:12 +0100

    change camelCase to underscore_variables
    Additionally re-ordering the writing of the conditional block
    of code.

--- winners.py
+++ winners.py
@@ -4,9 +4,9 @@ import csv, random, math
 from collections import Counter
 
 millnames = ['',' Thousand',' Million',' Billion',' Trillion']
-winningPercent = 0
-winCount = 0
-gambleCount = 0
+winning_percent = 0
+win_count = 0
+gamble_count = 0
 winnings = 2_000_000
 
 #by default we want to print out our progress every 1 million attempts
@@ -14,13 +14,7 @@ log_every_n = 1_000_000
 
 #uses randrange to generate a 5 index list with numbers ranging from 1 to 69
 def generate_mm_numbers():
-    #create a list
-    generated_nums = list()
-    # iterate 5 times
-    for x in range(0, 5):
-        #add a random number between 1 and 70
-        generated_nums.append(random.randrange(1, 71))
-    return generated_nums
+    return [random.randrange(1, 71) for x in range(5)]
 
 #uses randint to generate a number between 1 and 25
 def generate_mm_special_num():
@@ -42,12 +36,12 @@ def generate_mm_special_num():
         # winner_list = list(set(winner_list))
     # return winner_list
 
-def print_gamble_stats(winCount, gambleCount, winnings):
-    winningPercent = (( winCount / gambleCount ) * 100.00 )
-    print("Number of wins: "+str(winCount))
+def print_gamble_stats(win_count, gamble_count, winnings):
+    winning_percent = (( win_count / gamble_count ) * 100.00 )
+    print("Number of wins: "+str(win_count))
     print("Winnings: "+str(millify(winnings)))
-    print("Number of tries: "+str(millify(gambleCount)))
-    print("Winning percent: "+str(winningPercent))
+    print("Number of tries: "+str(millify(gamble_count)))
+    print("Winning percent: "+str(winning_percent))
     print("\n\n")
 
 def millify(n):
@@ -67,11 +61,11 @@ most_common_list = list()
 while winnings > 0.00:
     random_set = generate_mm_numbers()
     random_special_num = generate_mm_special_num()
-    matchingNum = 0
+    matching_num = 0
     matchingspecial = False
     winnings -= 2.00
-    gambleCount += 1
-    if gambleCount == 1:
+    gamble_count += 1
+    if gamble_count == 1:
         #option 1, interactive user input used to choose Mega Million numbers
         #chosen_nums = [int(x) for x in input("Enter five numbers between 1 and 69 separate each number by a space, then press enter:").split()]
         #chosen_special_num = input("Enter a number between 1 and 25, then press enter:")
@@ -87,7 +81,7 @@ while winnings > 0.00:
     for choice in random_set:
         running_list.append(choice)
         if choice in chosen_nums:
-            matchingNum += 1
+            matching_num += 1
         if chosen_special_num == random_special_num:
             matchingspecial = True
             running_special_list.append(random_special_num)
@@ -95,25 +89,28 @@ while winnings > 0.00:
             matchingspecial = False
 
     # winnings of $500 or more count as a win
-    if matchingNum > 3:
-        if matchingNum == 4 and not matchingspecial:
-            winnings += 500.00
-        elif matchingNum == 4 and matchingspecial:
-            winnings += 10_000.00
-            #print("you won 10,000 dollars!")
-            #print("\n\n")
-        elif matchingNum == 5 and not matchingspecial:
-            winnings += 1_000_000.00
-            print("you won 1,000,000 dollars!")
-            print("\n\n")
-        elif matchingNum == 5 and matchingspecial:
-            winnings += 1_600_000_000.00
-            print("you won the jackpot!!!!")
-            print(random_set, random_special_num)
-            print("\n\n")
-            print_gamble_stats(winCount, gambleCount, winnings)
-            exit()
-        winCount += 1
+    if matching_num > 3:
+        if matching_num == 4:
+            if matchingspecial:
+                winnings += 10_000.00
+                #print("you won 10,000 dollars!")
+                #print("\n\n")
+            else:
+                winnings += 500.00
+
+        elif matching_num == 5:
+            if matchingspecial:
+                winnings += 1_600_000_000.00
+                print("you won the jackpot!!!!")
+                print(random_set, random_special_num)
+                print("\n\n")
+                print_gamble_stats(win_count, gamble_count, winnings)
+                exit()
+            else:
+                winnings += 1_000_000.00
+                print("you won 1,000,000 dollars!")
+                print("\n\n")
+        win_count += 1
     #simple correction process. looks at the last 100,000 winning numbers, and changes chosen_nums to that list of numbers.
     # also resets the chosen_special_num based on which number was most commonly found in the past 100,00 iterations
     if len(running_list) > 100_000:
@@ -125,7 +122,8 @@ while winnings > 0.00:
         running_list = list()
         most_common_list = list()
 
-    if (gambleCount % log_every_n) == 0:
-        print_gamble_stats(winCount, gambleCount, winnings)
+    if (gamble_count % log_every_n) == 0:
+        print_gamble_stats(win_count, gamble_count, winnings)
 
-print_gamble_stats(winCount, gambleCount, winnings)
+if __name__ == "__main__":
+    print_gamble_stats(win_count, gamble_count, winnings)
